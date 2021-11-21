c13d769d27f58f28d80a9bdcd1b1df2abd42bf37
Alexandra Lupoi <alupoi18@gmail.com>
2021-11-21 11:22:07 +0200

    Tic-Tac-Toe

--- lib/tic_tac_toe.dart
+++ lib/tic_tac_toe.dart
@@ -28,98 +28,76 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tic-Tac-Toe'),
-        centerTitle: true,
-        backgroundColor: Colors.deepOrangeAccent,
-      ),
-      body: Column(
-        children: <Widget>[
+        appBar: AppBar(
+          title: const Text('Tic-Tac-Toe'),
+          centerTitle: true,
+          backgroundColor: Colors.deepOrangeAccent,
+        ),
+        body: Column(children: <Widget>[
           Expanded(
-            flex: 4,
-            child: GridView.builder(
-              padding: const EdgeInsets.all(20),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3
-              ),
-              itemCount: 9,
-              itemBuilder: (BuildContext context, int index) {
-                return GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      if(matrix[index] != 'X' && matrix[index] != '0') {
-                        matrix[index] = playerTurn ? 'X' : '0';
-                        if(!_check()) {
-                          playerTurn = !playerTurn;
-                        }
-                      }
-                    });
-                  },
-                  child: AnimatedContainer(
-                    duration: const Duration(seconds: 3),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.orangeAccent)
-                    ),
-                    child: Center(
-                      child: Text(
-                        matrix[index],
-                        style: const TextStyle(
-                          fontSize: 28,
-                          color: Colors.green
-                        )
-                      )
-                    )
-                  )
-                );
-              }
-            )
-          ),
-          if(_check() || !matrix.contains(''))
+              flex: 4,
+              child: GridView.builder(
+                  padding: const EdgeInsets.all(20),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+                  itemCount: 9,
+                  itemBuilder: (BuildContext context, int index) {
+                    return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            if (matrix[index] != 'X' && matrix[index] != '0') {
+                              matrix[index] = playerTurn ? 'X' : '0';
+                              if (!_check()) {
+                                playerTurn = !playerTurn;
+                              }
+                            }
+                          });
+                        },
+                        child: AnimatedContainer(
+                            duration: const Duration(seconds: 3),
+                            decoration: BoxDecoration(border: Border.all(color: Colors.orangeAccent)),
+                            child: Center(
+                                child:
+                                    Text(matrix[index], style: const TextStyle(fontSize: 28, color: Colors.green)))));
+                  })),
+          if (_check() || !matrix.contains(''))
             Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
+                child: Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              ElevatedButton(
                   onPressed: () {
                     setState(() {
                       matrix = List<String>.filled(9, '');
                     });
                   },
-                  child: const Text('Play again!')
-                )
-              ]
-            )
-          )
-        ]
-      )
-    );
+                  child: const Text('Play again!'))
+            ]))
+        ]));
   }
 
   bool _check() {
     // Check rows
-    for(int index = 0; index < 9; index += 3) {
-      if(matrix[index] != '') {
-        if(matrix[index] == matrix[index + 1] && matrix[index + 1] == matrix[index + 2]) {
+    for (int index = 0; index < 9; index += 3) {
+      if (matrix[index] != '') {
+        if (matrix[index] == matrix[index + 1] && matrix[index + 1] == matrix[index + 2]) {
           return true;
         }
       }
     }
 
     // Check columns
-    for(int index = 0; index < 3; index++) {
-      if(matrix[index] != '') {
-        if(matrix[index] == matrix[index + 3] && matrix[index + 3] == matrix[index + 6]) {
+    for (int index = 0; index < 3; index++) {
+      if (matrix[index] != '') {
+        if (matrix[index] == matrix[index + 3] && matrix[index + 3] == matrix[index + 6]) {
           return true;
         }
       }
     }
 
     //Check diagonals
-    if(matrix[4] != '') {
-      if(matrix[0] == matrix[4] && matrix[4] == matrix[8]) {
+    if (matrix[4] != '') {
+      if (matrix[0] == matrix[4] && matrix[4] == matrix[8]) {
         return true;
       }
-      if(matrix[2] == matrix[4] && matrix[4] == matrix[6]) {
+      if (matrix[2] == matrix[4] && matrix[4] == matrix[6]) {
         return true;
       }
     }
